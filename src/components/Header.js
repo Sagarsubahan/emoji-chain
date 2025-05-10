b5659f89e34d34a61fa968a3943ef893601c31ff
Sagarsubahan <93981306+Sagarsubahan@users.noreply.github.com>
2025-05-10 18:30:41 +0530

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ const Header = () => (
   <header
     className="fixed top-0 left-0 right-0 bg-[#181f2a] p-4 z-40 shadow flex justify-end items-center"
     style={{
-      minHeight: 64,
+      minHeight: 100,
       borderBottom: "1px solid #232b3a",
     }}
   >
